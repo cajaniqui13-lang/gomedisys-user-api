cab88ac6f33eeba747a8ff326a454c42ab3e8531
cajaniqui13-lang <cajaniqui13@gmail.com>
2026-07-24 10:26:41 -0500

    Estructura driver.py

--- src/driver.py
+++ src/driver.py
@@ -1,1 +1,25 @@
-get_driver()
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from config import HEADLESS
+
+
+def get_driver():
+    """
+    Crea y devuelve una instancia configurada de Chrome.
+    """
+
+    options = Options()
+
+    # Mantiene la ventana abierta al finalizar el script
+    options.add_experimental_option("detach", True)
+
+    # Ejecutar sin interfaz gráfica (opcional)
+    if HEADLESS:
+        options.add_argument("--headless=new")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.maximize_window()
+
+    return driver
